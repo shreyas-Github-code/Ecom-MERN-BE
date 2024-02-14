bdb328979bafa8fdca2e999b391e57df6fbbbcb2
shreyas-Github-code <mishrikotishreyas@gmail.com>
2024-02-14 19:15:46 +0530

    user model partial

--- dist/models/user.js
+++ dist/models/user.js
@@ -1,3 +1,22 @@
 import mongoose from "mongoose";
-const schema = new mongoose.Schema({}, { timestamps: true, });
+import validator from "validator";
+const schema = new mongoose.Schema({
+    _id: { type: String, required: [true, "Plese enter ID"] },
+    name: { type: String, required: [true, "Plese enter Name"] },
+    email: { type: String, unique: [true, "Email Alredy Exist"], required: [true, "Plese enter Email"], validate: validator.default.isEmail },
+    photo: { type: String, required: [true, "Plese add Photo"] },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    gender: { type: String, enum: ["male", "female"], required: [true, "Plese enter Gender"] },
+    dob: { type: Date, required: [true, "Plese enter Your Date of birth"] },
+}, { timestamps: true, });
+schema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+    if (today.getMonth() < dob.getMonth() ||
+        today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate()) {
+        age--;
+    }
+    return age;
+});
 export const User = mongoose.model("User", schema);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,44 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
+interface IUser extends Document {
+    _id:string;
+    name:string;
+    email:string;
+    photo:string;
+    role:"admin"|"user";
+    gender:"male"|"femail";
+    dob:Date,
+    createdAt:Date;
+    updatedAt:Date;
+    age:number;
+}
 
-const schema = new mongoose.Schema({},{timestamps:true,});
+const schema = new mongoose.Schema(
+    {
+    _id: { type: String, required: [true, "Plese enter ID"] },
+    name: { type: String, required: [true, "Plese enter Name"] },
+    email: { type: String, unique: [true, "Email Alredy Exist"],required: [true, "Plese enter Email"],validate:validator.default.isEmail},
+    photo: { type: String, required: [true, "Plese add Photo"] },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    gender: { type: String, enum: ["male", "female"], required: [true, "Plese enter Gender"]},
+    dob: { type: Date, required: [true, "Plese enter Your Date of birth"]},
 
-export const User = mongoose.model("User",schema);
+}, 
+{ timestamps: true, });
+
+schema.virtual("age").get(function(){
+    const today = new Date();
+    
+    const dob = this.dob;
+    let age = today.getFullYear()-dob.getFullYear();
+
+    if(today.getMonth()<dob.getMonth() || 
+    today.getMonth()===dob.getMonth() && today.getDate()<dob.getDate())
+    {
+        age--;
+    }
+        return age;
+    
+});
+export const User = mongoose.model("User", schema);
